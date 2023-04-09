e3c28a7916ef53fb354c2f473da0978bafe93634
Morven Hou <morven.hou@viljasolutions.com>
2023-04-10 07:09:51 +0800

    feat: 🎉 initialize format configurations

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
